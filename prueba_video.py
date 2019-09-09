b7e6bdc92555462d8dc4202b07561019b6b8484d
andres <velasco.andrs@gmail.com>
2019-09-09 08:26:11 -0500

    cambio de nombre a las variables en codigo del video

--- prueba_video.py
+++ prueba_video.py
@@ -1,51 +1,46 @@
+from builtins import ord
 from keras.preprocessing.image import img_to_array
 from keras.models import load_model
-from imutils.video import VideoStream
-from threading import Thread
 import numpy as np
-import imutils
-import time
 import cv2
-import os
 
 
 MODEL_PATH = "red_v3.h5"
 
-SANTA = False
-# load the model
+# cargar el modelo
 print("Cargando la red neural...")
 model = load_model(MODEL_PATH)
-# initialize the video stream and allow the camera sensor to warm up
+# inicializar erl video
 print("Cargando Video")
-video = cv2.VideoCapture('videos_prueba/video3.mp4')
+video = cv2.VideoCapture('videos_prueba/video1.mp4')
 
 while True:
-    q, frame = video.read()
-    # prepare the image to be classified by our deep learning network
-    image = cv2.resize(frame, (28, 28))
-    image = image.astype("float") / 255.0
-    image = img_to_array(image)
-    image = np.expand_dims(image, axis=0)
-    # classify the input image and initialize the label and
-    # probability of the prediction
-    (vacio, lleno) = model.predict(image)[0]
-    print(model.predict(image))
-    label = "No lleno"
-    proba = vacio
-    if lleno > vacio:
-        proba = lleno
-        label = "Lleno"
+    q, cuadro = video.read()
+    # preparamos la imagen para que pueda ser compatible con la red neural
+    imagen = cv2.resize(cuadro, (28, 28))
+    imagen = imagen.astype("float") / 255.0
+    imagen = img_to_array(imagen)
+    imagen = np.expand_dims(imagen, axis=0)
+    # clasificamos la imagen obtenemos las probabilidades
+    (probabilidadVacio, probabilidadLleno) = model.predict(imagen)[0]
+    etiqueta = "No lleno"
+    probabilidad = probabilidadVacio
+    # etiquetamos la imagen conforme a la prediccion mas alta
+    if probabilidadLleno > probabilidadVacio:
+        probabilidad = probabilidadLleno
+        etiqueta = "Lleno"
 
-    label = "{}: {:.2f}%".format(label, proba * 100)
-    frame = cv2.putText(frame, label, (10, 25),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+    # armamos la etiqueta con su respectiva probabilidad y la ponemos en la imagen
+    etiqueta = "{}: {:.2f}%".format(etiqueta, probabilidad * 100)
+    cuadro = cv2.putText(cuadro, etiqueta, (10, 25),
+                         cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
 
-    # show the output frame
-    cv2.imshow("Cuadro", frame)
-    key = cv2.waitKey(1) & 0xFF
+    # mostrar la imagen
+    cv2.imshow("Video", cuadro)
+    teclaPresionada = cv2.waitKey(1) & 0xFF
 
     # if the `q` key was pressed, break from the loop
-    if key == ord("q"):
+    if teclaPresionada == ord("q"):
         break
 
 cv2.destroyAllWindows()
